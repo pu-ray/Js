25278e7f148edf923215f9608e580160c7d14464
mbuguapurity <mbuguapurity1@gmail.com>
2019-07-14 22:53:08 +0300

    new files

--- assignment 13/client/index.js
+++ assignment 13/client/index.js
@@ -1,3 +1,9 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import './index.css'
+// import App from './App'
+
+
 
 fetch("http://127.0.0.1:5000/products")
 .then(response => response.json())
@@ -11,28 +17,30 @@ fetch("http://127.0.0.1:5000/products")
          })
         })
 
-        fetch("http://127.0.0.1:5000/products/fruits")
-            .then(response => response.json())
-            .then(response => {
-                response.forEach(item => {
-                    var node = document.createElement("li");
-                    var textnode = document.createTextNode(item.name);
-                    node.appendChild(textnode);
-                    document.getElementById("fruList").appendChild(node);
+fetch("http://127.0.0.1:5000/products/fruits")
+.then(response => response.json())
+.then(response => {
+     response.forEach(item => {
+     var node = document.createElement("li");
+     var textnode = document.createTextNode(item.name);
+     node.appendChild(textnode);
+     document.getElementById("fruList").appendChild(node);
                     
-                     })
-                    })
-                    
-                    fetch("http://127.0.0.1:5000/products/vegetables")
-                    .then(response => response.json())
-                    .then(response => {
-                        response.forEach(item => {
-                            var node = document.createElement("li");
-                            var textnode = document.createTextNode(item.name);
-                            node.appendChild(textnode);
-                            document.getElementById("vegList").appendChild(node);
-                            
-                             })
-                            })
+     })
+     })
 
                     
+fetch("http://127.0.0.1:5000/products/vegetables")
+.then(response => response.json())
+.then(response => {
+    response.forEach(item => {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(item.name);
+    node.appendChild(textnode);
+    document.getElementById("vegList").appendChild(node);
+                            
+    })
+    })
+
+ 
+    // ReactDOM.render(<App />, document.getElementById('root'))                  
